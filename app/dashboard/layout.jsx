c355971f2f5faeb2bcfcf373b7d5b8fce68ede0d
nandiop <su.nandi2002@gmail.com>
2025-04-22 18:31:25 +0530

    fixing errors

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,15 +1,16 @@
 import React, { Suspense } from 'react'
 import { ClipLoader } from 'react-spinners'
 
-export  const Layout = ({ children, showHeading = true }) => {
+export default function DashboardLayout({ children, showHeading = true }) {
   return (
     <div className='w-full flex flex-col items-center align-middle'>
-        {showHeading && (
-          <div className='flex items-center justify-between mb-5'>
-          
-          </div>
-        )}
-        <Suspense fallback={
+      {showHeading && (
+        <div className='flex items-center justify-between mb-5'>
+          {/* Optional heading content here */}
+        </div>
+      )}
+      <Suspense
+        fallback={
           <div className="flex flex-wrap align-middle items-center justify-center min-h-[600px] space-y-4">
             <div className="animate-spin">
               <ClipLoader
@@ -20,13 +21,11 @@ export  const Layout = ({ children, showHeading = true }) => {
                 aria-label="Loading Spinner"
               />
             </div>
- 
           </div>
-        }>
-          {children}
-        </Suspense>
+        }
+      >
+        {children}
+      </Suspense>
     </div>
   )
 }
-
-
